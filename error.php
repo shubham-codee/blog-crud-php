c4769cb28e7fdecd8c8f8ed98d3e423d32c341c7
Shubham Mohapatra <rahultapu07@gmail.com>
2025-05-07 11:54:32 +0530

    added error page

---
+++ error.php
@@ -0,0 +1,14 @@
+<?php
+include 'header.php';
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="text-center">
+            <h1 class="display-1 text-danger">Oops!</h1>
+            <p class="lead text-muted">Sorry, something went wrong on our end. We're working on it!</p>
+    </div>
+</div>
+</div>
+
+<?php include 'footer.php'; ?>
